31fb3d2f63a9a0564da63ec6c6ecb155910097b4
Jacob Ross <jross1818@gmail.com>
2019-07-01 18:05:07 -0500

    update files 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Jacob Ross is a boss.
           </p>
           <a
             className="App-link"
